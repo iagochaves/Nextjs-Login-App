edfe520e1eaa86497a9aec327f395af866c2bf8a
iagochaves <iagovieirachaves@gmail.com>
2022-06-12 22:26:11 -0300

    Adding page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ReactNode } from 'react';
+
+type AppLayoutProps = {
+  children: ReactNode;
+};
+
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => (
+  <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {children}
+  </div>
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
